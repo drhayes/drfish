5b72d6c2dd918eab63acf292178053503b74ceeb
David Hayes <drhayes@gmail.com>
2020-10-05 13:45:40 -0500

    Make gl.fish take arguments.

--- functions/gl.fish
+++ functions/gl.fish
@@ -1,3 +1,3 @@
 function gl --description "git log --awesome"
-  git log --graph --date=relative --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%n%w(0,4,4)%-b%n%n%-N'
+  git log --graph --date=relative --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset%n%w(0,4,4)%-b%n%n%-N' $argv
 end
