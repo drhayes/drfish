5c066bde3b4f35feac6442a142f3e95bc055146e
David Hayes <drhayes@gmail.com>
2021-02-26 13:58:14 -0600

    Update glgpas function to use bw.

--- functions/glgpass.fish
+++ functions/glgpass.fish
@@ -1,3 +1,9 @@
-function glgpass
-  lpass show glg-sso-login --password | tr -d '\n' | kitty +kitten clipboard
+function glgpass --description "Grab my SSO password from storage and put it in on my clipboard."
+  if test -z $GLG_SSO_ITEM_ID
+    set_color red
+    echo "Please set GLG_SSO_ITEM_ID first."
+    set_color normal
+    return 1
+  end
+  bw get item $GLG_SSO_ITEM_ID | jq --raw-output '.login.password' | tr --delete '\n' | kitty +kitten clipboard
 end
