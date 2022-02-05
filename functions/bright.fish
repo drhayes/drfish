88238b5a282d95fe92c235abf43f49f9ee75c57d
David Hayes <drhayes@gmail.com>
2022-02-05 12:57:42 -0600

    Removing extraneous grep -v from bright.

--- functions/bright.fish
+++ functions/bright.fish
@@ -1,5 +1,5 @@
 function bright --description "Make my screens brighter."
-  set displays (xrandr | grep connected | grep -v disconnected | cut -f1 -d' ')
+  set displays (xrandr | grep ' connected' | cut -f1 -d' ')
   for display in $displays
     xrandr --output $display --brightness 1
   end
