6cb8ed8af21e8b421e65e3353b2c89c6ff0e25e9
David Hayes <drhayes@gmail.com>
2022-03-28 03:35:39 +0000

    Add owner to the path when cloning.

--- functions/ghclone.fish
+++ functions/ghclone.fish
@@ -1,7 +1,7 @@
 function ghclone -a repo thingy --description "Clone a repo from GitHub to ~/src."
   set -l default_path (string split --max 1 --right / $repo)
   if test -z "$thingy"
-    set thingy $default_path[2]
+    set thingy $default_path
   end
   cd ~/src
   git clone git@github.com:$repo $thingy
