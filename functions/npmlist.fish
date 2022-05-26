c57ccc8ad48db30930b3fced16981f5f15e96fd6
David Hayes <drhayes@hey.com>
2022-05-26 10:57:00 -0500

    Utility for listing npm scripts section.

---
+++ functions/npmlist.fish
@@ -0,0 +1,11 @@
+function npmlist --description "List the scripts section in package.json."
+  if test ! -e package.json
+    set_color red
+    echo "No package.json file found in current directory."
+    set_color normal
+    return 1
+  end
+
+  cat package.json | jq .scripts
+end
+
