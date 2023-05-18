70668defbb44b39495eea28dc4b31acc935bcb0c
David Hayes <drhayes@omg.lol>
2023-05-18 16:37:29 -0500

    Make ghclone do a "git pull" if I already have the repo.

--- functions/ghclone.fish
+++ functions/ghclone.fish
@@ -1,14 +1,16 @@
 function ghclone -a repo thingy --description "Clone a repo from GitHub to ~/src."
-  set -l default_path $repo
-  if test -z "$thingy"
-    set thingy $default_path
-  end
-  # Am I forgetting that I already cloned that? If so, just send me there.
-  if test -d $HOME/src/$thingy
-    cd $HOME/src/$thingy
-    return
-  end
-  cd $HOME/src
-  git clone git@github.com:$repo $thingy
-  cd $thingy
+    set -l default_path $repo
+    if test -z "$thingy"
+        set thingy $default_path
+    end
+    # Am I forgetting that I already cloned that? If so, just send me there.
+    # But go ahead and update it because why not.
+    if test -d $HOME/src/$thingy
+        cd $HOME/src/$thingy
+        git pull
+        return
+    end
+    cd $HOME/src
+    git clone git@github.com:$repo $thingy
+    cd $thingy
 end
