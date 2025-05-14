fcdf2f75371ba9d3532f0092db2c14b28c808fdf
David Hayes <drhayes@omg.lol>
2025-05-14 14:35:34 -0500

    Don't replace pbcopy/pbpaste if they exist.

--- functions/pbcopy.fish
+++ functions/pbcopy.fish
@@ -1,3 +1,5 @@
-function pbcopy --description "Copy to clipboard from command-line using kitty."
-  kitty +kitten clipboard
+if not test -e /usr/bin/pbcopy
+    function pbcopy --description "Copy to clipboard from command-line using kitty."
+        kitty +kitten clipboard
+    end
 end
--- functions/pbpaste.fish
+++ functions/pbpaste.fish
@@ -1,3 +1,5 @@
-function pbpaste --description "Paste to terminal from clipboard using kitty."
-  kitty +kitten clipboard --get-clipboard
+if not test -e /usr/bin/pbpaste
+    function pbpaste --description "Paste to terminal from clipboard using kitty."
+        kitty +kitten clipboard --get-clipboard
+    end
 end
